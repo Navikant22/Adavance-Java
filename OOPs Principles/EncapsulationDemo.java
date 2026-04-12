38ce568a0f15171e65e59d12e580640155562a52
Navikant22 <142095492+Navikant22@users.noreply.github.com>
2026-04-12 12:38:44 +0530

    Implement encapsulation in Student class

--- OOPs Principles/EncapsulationDemo.java
+++ OOPs Principles/EncapsulationDemo.java
@@ -1,1 +1,36 @@
+class Student {
+    private String name;
+    private int age;
 
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter
+    public int getAge() {
+        return age;
+    }
+
+    // Setter
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        }
+    }
+}
+
+public class EncapsulationDemo {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Navi");
+        s.setAge(21);
+
+        System.out.println(s.getName() + " " + s.getAge());
+    }
+}
